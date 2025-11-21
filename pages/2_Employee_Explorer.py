1b934e7b702f6c8cadc4e653ca33b80a92a8879b
engkinandatama <engkinandatama@outlook.com>
2025-11-22 01:53:33 +0700

    all init

---
+++ pages/2_Employee_Explorer.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("🎯 Employer Explorer")
+st.write("Halaman ini akan berisi Employer Explorer .")
---
+++ pages/4_AI_Job_Generator.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("🎯 Talent Matching")
+st.write("Halaman ini akan berisi Talent Match Engine.")
